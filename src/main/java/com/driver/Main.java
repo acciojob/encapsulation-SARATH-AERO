8b3f36507c889d65fcca9fa5b5b83ea6d93281a0
Sarath Sekar <sarathaero1@gmail.com>
2023-03-23 14:16:17 +0530

    encapsulation problem solves

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        r.setName("rashmika");
+        System.out.println(r.getName());
+//        'name' has private access in 'com.driver.RWOnly'
+    }
 }
